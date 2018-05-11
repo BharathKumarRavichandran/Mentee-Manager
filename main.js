709e9f04777cd1829a9cbab25188ccf8b0ac382e
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-05-11 22:36:35 +0530

    messed up crap

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var addButton = document.getElementById("addButton");
+var commentButton = document.getElementById("commentButton");
+var editButton = document.getElementById("editButton");
+var deleteButton = document.getElementById("deleteButton");
 var heading = document.getElementById("heading");
 var list = document.getElementsByClassName("list");
 var menteeRegion = document.getElementById("menteeRegion");
@@ -7,6 +10,7 @@ var mBoxId = document.getElementById("mBoxId");
 var mBoxInputId = document.getElementById("mBoxInputId");
 var mBox = document.getElementsByClassName("mBox");
 var mBoxInput = document.getElementsByClassName("mBoxInput");
+var mcId = document.getElementById("mcId");
 var mCommentId = document.getElementById("mCommentId");
 var mName = document.getElementsByClassName("mName");
 var mComment = document.getElementsByClassName("mComment");
@@ -24,6 +28,7 @@ var menteeComment = new Array(); //Array which contains the comment on each ment
 var menteeRating = new Array(); //Array which contains the rating oon each mentee
 var i=0;//No. of mentees
 var j=0;
+var k=0;
 heading.style.marginLeft = (w-w/2.5)+"px";//To centre the heading
 addButton.style.marginLeft = (w-w/13)+"px"; //To centre the element
 mBoxInputId.style.marginLeft = (w-w/6)+"px"; //To centre the element
@@ -45,7 +50,6 @@ function myFunction() {
     var list = document.getElementById("myList");
     list.insertBefore(newItem, list.childNodes[0]);
 }*/
-var mcId = document.getElementById("mcId");
 
 function newMentee(){
 	j=0;
@@ -54,18 +58,25 @@ function newMentee(){
 	menteeComment[i][j] = mCommentId.value;
 	menteeRating[i] = rating.value;
 	nameId.value="Mentee's Name";
-	mCommentId.value="Write a comment..";
+	mCommentId.value="Write a comment...";
 	menteeRegionInput.removeChild(mBoxInputId);
 	mentee[i] = mBoxId.cloneNode(true);
 	mentee[i].setAttribute("id","mBoxId"+i);
+	var p = mentee[i].id;
+	alert(document.getElementById(p));
 	var mBoxChild = mentee[i].childNodes;
-	alert(mBoxChild[12]);
+	//alert(mBoxChild[12]);
 	//mBoxChild[12].setAttribute("id","commentButton"+i);
+	mBoxId.id = "mBoxId"+i;
+	commentButton.id = "commentButton"+i;
+	commentButton.setAttribute("id","commentButton"+i);
+	alert(document.getElementById("commentButton"+i));
+	mcId.id ="mcId"+i;
+	editButton.id = "editButton"+i;
+	deleteButton.id = "deleteButton"+i;
 	/*.setAttribute("id","mName"+i);
 	.setAttribute("id","mStar"+i);
 	.setAttribute("id","listId"+i);
-	.setAttribute("id","mcId"+i);
-	.setAttribute("id","commentButton"+i);*/
 /* 
  var starNodes = mStar.childNodes;
  for(j=0;j<menteeRating[i];j++){
@@ -92,11 +103,28 @@ function newMentee(){
 }
 
 function newComment(){
-	i=0;
-	menteeComment[i][j] = mcId.value;
+	k = commentButton.id[13];//To get the mentee number
+	var mcIdnew = mcId.id;
+	alert(mcIdnew);
+	menteeComment[k][j] = mcIdnew.value;
 	var newItem = document.createElement("LI");
-	var textnode = document.createTextNode(menteeComment[i][j]);
+	var textnode = document.createTextNode(menteeComment[k][j]);
 	newItem.appendChild(textnode);
-	list[i].appendChild(newItem);
+	list[k].appendChild(newItem);
+	var y = document.getElementById("mcIdnew");
+	//alert(y);
+	y.value="Write a comment...";
+	//alert(mcId.id.value);
 	j++;
+}
+
+function editBox(){
+	k = editButton.id[10];//To get the mentee number
+}
+
+function deleteBox(){
+	k = deleteButton.id[12];//To get the mentee number
+	mBoxIdnew = "mBoxId"+k;
+	var x = document.getElementById(mBoxIdnew);
+	x.parentNode.removeChild(x);
 }
